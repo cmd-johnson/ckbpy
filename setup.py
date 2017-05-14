bafdaa1aaa475313147bd9decbedd9b457bbb79b
Jonas Auer <jonas.auer.94@gmail.com>
2017-05-14 13:58:01 +0200

    Add files used to publish to pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ckbpy',
+      version='0.0.0',
+      description='Write ckb effects using Python!',
+      url='http://github.com/cmd-johnson/ckbpy',
+      author='Jonas Auer',
+      author_email='jonas.auer.94@gmail.com',
+      license='GPL-2.0',
+      packages=['ckbpy'],
+      zip_safe=True)
