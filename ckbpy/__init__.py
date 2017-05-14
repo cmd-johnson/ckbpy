d98ae66efa26da51cf3f2bf0bd8002ad56e67c8c
Jonas Auer <jonas.auer.94@gmail.com>
2017-05-14 13:55:42 +0200

    Add constants to the types exported by __init__

--- ckbpy/__init__.py
+++ ckbpy/__init__.py
@@ -3,3 +3,4 @@ from .types import (RGBColor, ARGBColor,
                     GradientColorStops, AGradientColorStops)
 from .params import (Long, Double, Bool, RGB, ARGB, Gradient, AGradient, Angle,
                      String, Label)
+from .constants import Keypress, Time
