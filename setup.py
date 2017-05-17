621993351d7ca8e75fc9272b66975e58958069cd
Jonas Auer <jonas.auer.94@gmail.com>
2017-05-17 13:08:15 +0200

    Bump version to 0.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ckbpy',
-      version='0.0.0',
+      version='0.0.1',
       description='Write ckb effects using Python!',
       url='http://github.com/cmd-johnson/ckbpy',
       author='Jonas Auer',
